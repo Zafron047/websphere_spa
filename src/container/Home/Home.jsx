e805875a193ebbe48bda9142480e56aa083b16ca
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2024-12-28 14:10:06 +0600

    Add Home section

---
+++ src/container/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+  return (
+      <div className='container'>
+        <div className="content">
+          <h1>Elevating UX Transformation with</h1>
+          <h1 className="gradient-header">Business Solutions</h1>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It
+            has roots in a piece of classical Latin literature from making it over
+            2000 years old. Richard McClintock, a Latin professor at
+            Hampden-Sydney College in Virginia.
+          </p>
+          <button className="button">Contact Us</button>
+        </div>
+      </div>
+  );
+};
+
+export default Home;
