bab409256e7a57590867f398c8820e2ec5e0636c
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2024-12-30 15:51:51 +0600

    Add images for home section

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import behance from '../assets/behance.png';
+import clutch from '../assets/clutch.png';
+import manifest from '../assets/manifest.png';
+import agency_logo from '../assets/agency_logo.png';
+
+export default {
+  behance,
+  clutch,
+  manifest,
+  agency_logo,
+};
