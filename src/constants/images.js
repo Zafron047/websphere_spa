ef396848fe51f39ee7892e71b54329187ca43675
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2024-12-31 18:55:03 +0600

    Add images and update export path registry

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,10 +2,32 @@ import behance from '../assets/behance.png';
 import clutch from '../assets/clutch.png';
 import manifest from '../assets/manifest.png';
 import agency_logo from '../assets/agency_logo.png';
+import about_office from '../assets/about_office.png';
+import adidas from '../assets/adidas.png';
+import american_apparel from '../assets/american_apparel.png';
+import design from '../assets/design.png';
+import diversity_zoom from '../assets/diversity_zoom.png';
+import google from '../assets/google.png';
+import nike from '../assets/nike.png';
+import person from '../assets/person.png';
+import ui_ux from '../assets/ui_ux.png';
+import thumbnail from '../assets/thumbnail.png';
 
-export default {
+const images = {
   behance,
   clutch,
   manifest,
   agency_logo,
+  about_office,
+  adidas,
+  american_apparel,
+  design,
+  diversity_zoom,
+  google,
+  nike,
+  person,
+  ui_ux,
+  thumbnail
 };
+
+export default images;
