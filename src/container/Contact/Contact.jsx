5f9cf20e24b3a2ece292465f54a830c669407774
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2025-01-01 23:46:20 +0600

    Add Contact section Remove test borders and fixed heights.

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { BsTelephoneInboundFill } from 'react-icons/bs';
+
+import images from '../../constants/images';
+import './Contact.scss';
 
 const Contact = () => {
   return (
-    <div>Contact H:1148</div>
-  )
-}
+    <div className="contact__container">
+      <div className="contact-first">
+        <p>Got a project in mind?</p>
+        <h2>Let’s discuss the <br/>details</h2>
+        <button
+          // className="app__button"
+          style={{ color: 'white', background: '#030307', width: '150px', height: '50px', fontSize: '0.75rem' }}
+        >
+          Contact Us
+        </button>
+      </div>
+      <div className="contact-last">
+        <img src={images.person} alt="profile pic" />
+        <p>
+          Stay informed and inspi by joining our newsletter! Sign up today to
+          receive ux design the.
+        </p>
+        <button className="app__button">
+          Ask Question <BsTelephoneInboundFill className="telephone" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
